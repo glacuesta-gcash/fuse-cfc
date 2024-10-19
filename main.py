1190d4394d5d30737215197e77c2885bd1dcaa7f
G Lacuesta (Jerry) <gabby.lacuesta.dev@gmail.com>
2024-10-19 15:44:53 +0800

    multiple sheet ids

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ import os
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
+id_orig =       '1abQSainHd7j44v2Wq8EToCS_5v22rMekwJcUu19mjtE'
+id_base =       '1s0Cnb5o2vbXAYZinCbsMEYx5vIS7JlHrxAsVB2cjqtU'
+id_aggressive = '1MB_DtVpHV5wImG3_qUj6nwdwxS72zta-wD4D8URe2NY'
+
 timer = Timer()
-sheet = Sheet('1abQSainHd7j44v2Wq8EToCS_5v22rMekwJcUu19mjtE')
+sheet = Sheet(id_aggressive)
 
 cmd = sheet.steps_tab.read_next_command()
 while cmd is not None:
